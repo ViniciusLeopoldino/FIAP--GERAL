c3636b9beb9fe3aaa129cb29cac94fb0abebe626
ViniciusLeopoldino <135030968+ViniciusLeopoldino@users.noreply.github.com>
2024-04-25 21:14:10 -0300

    att nome

---
+++ FRONT-END DESIGN ENGINEERING/portoAutoCheck/javaScript/registro.js
@@ -0,0 +1,5 @@
+const registerButton = document.getElementById("saveButton");
+registerButton.addEventListener("click", function() {
+    // Redirecionar para a página de login
+    window.location.href = "login.html";
+});
