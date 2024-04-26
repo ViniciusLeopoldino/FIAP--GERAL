154c98765d63a405a5900d9268394bcc9a7281a8
ViniciusLeopoldino <135030968+ViniciusLeopoldino@users.noreply.github.com>
2024-04-25 21:15:04 -0300

    Delete FRONT-END DESIGN ENGINEERING/meuPorto directory
    atualização da pasta

--- FRONT-END DESIGN ENGINEERING/meuPorto/javaScript/registro.js
+++
@@ -1,5 +0,0 @@
-const registerButton = document.getElementById("saveButton");
-registerButton.addEventListener("click", function() {
-    // Redirecionar para a página de login
-    window.location.href = "login.html";
-});
